f5ddcedba114d1105dbd8bd2c5c8e7119909d235
Harish <harish.m@diggibyte.com>
2024-10-14 21:35:00 +0530

    df function, str function, array

---
+++ pyspark/dataframefunction.py
@@ -0,0 +1,39 @@
+from pyspark.python.pyspark.shell import spark
+from pyspark.sql import SparkSession
+
+dff=SparkSession.builder \
+    .appName("rfg") \
+    .getOrCreate()
+
+eg=spark.sparkContext
+
+a=[(1,"hari"),(2,"bharath"),(3,"tharun"),(4,"pranav")]
+a2=["id","name"]
+a1=spark.createDataFrame(data=a,schema=a2)
+s=a1.select("name").show()
+
+# from pyspark.shell import spark
+# from pyspark.sql import SparkSession
+# from pyspark.sql.functions import when
+# from setuptools.command.alias import alias
+#
+# spark = SparkSession.builder \
+#     .appName("name") \
+#     .getOrCreate()
+# sc = spark.sparkContext
+#
+# data = [(1, "Alice", "laptop"), (2, "Bob", "phone"), (3, "Charlie", "laptop"), (4, "David", "botle"), (5, "Eve", "cap")]
+# columns = ["id", "name", "product"]
+# df = spark.createDataFrame(data, columns)
+#
+# # Dataframe Functions:
+#
+# # res=df.select("name").show()    --select
+# # res=df.filter(df["id"]>1).show()  --Filter
+# # res=df.where(df["id"]<3).show()   --Where
+# # res=df.filter(df["name"].like("%e")).show() --LIKE
+#
+# # res=df.withColumn('Salary',when(df["id"]==1,"1000").otherwise("2000")).show() ---When & Otherwise
+# # res=df.describe().show() ----describe()
+# # res=df.groupBy("product").sum("id").orderBy("product",desc).show()
+#
